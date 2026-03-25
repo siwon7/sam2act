b12379cd0ee83afe8911b25c5b7a05118b031ed2
hyuncion <hsj489startup@gmail.com>
2026-03-25 17:00:45 +0900

    Use 8-bit bitsandbytes LAMB when enabled

--- sam2act/models/sam2act_agent.py
+++ sam2act/models/sam2act_agent.py
@@ -420,6 +420,7 @@ class SAM2Act_Agent:
                     lr=self._lr,
                     weight_decay=self._lambda_weight_l2,
                     betas=(0.9, 0.999),
+                    optim_bits=8,
                 )
             else:
                 # From: https://github.com/cybertronai/pytorch-lamb/blob/master/pytorch_lamb/lamb.py
